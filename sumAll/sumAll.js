dfafd736d9c1208f5e9ee757b77526a138671e91
Keener <40210883+KeenerCoder@users.noreply.github.com>
2021-05-11 01:45:19 -0600

    Add more files with solutions

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+const sumAll = function (integer1, integer2) {
+
+   // get any errors dealt with
+
+   if (integer1 < 0 || integer2 < 0) {
+      return "ERROR";
+   }
+
+   // if (isNaN(integer1) || isNaN(integer2)) {
+   //    return "ERROR";
+   // }
+
+   if (typeof (integer1) != "number" || typeof (integer2) != "number") {
+      return "ERROR";
+   }
+   // No errors then we can safely proceed
+
+   let totalSum = 0;
+   let startingInteger = integer1;
+   let endingInteger = integer2;
+
+   if (integer1 > integer2) {
+      startingInteger = integer2;
+      endingInteger = integer1;
+   }
+   for (let i = startingInteger; i <= endingInteger; i++) {
+
+      totalSum += i;
+   }
+   return totalSum;
 
 }
 
