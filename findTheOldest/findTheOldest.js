65e20513ee3bb18ad965315c85dc4b956dbb189e
Keener <40210883+KeenerCoder@users.noreply.github.com>
2021-06-01 17:01:22 -0600

    Update files to pass spec tests

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,8 +1,41 @@
 let findTheOldest = function (people) {
-   people.forEach(person => {
+   let oldestPerson = people.reduce((oldest, currentPerson) => {
+      console.log(oldest);
+      if (getPersonAge(oldest.yearOfBirth, oldest.yearOfDeath) >
+         getPersonAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)) {
+         return oldest;
+      }
+      else {
+         return currentPerson;
+      }
 
    });
+   return oldestPerson;
+};
 
-}
+const getPersonAge = function (birthYear, deathYear) {
+   if (!deathYear) {
+      deathYear = new Date().getFullYear();
+   }
+   return deathYear - birthYear;
+};
+
+
+// const findTheOldest = function (ppl) {
+//    return ppl.reduce((oldest, current) => {
+//       if (getAge(oldest.yearOfBirth, oldest.yearOfDeath) > getAge(current.yearOfBirth, current.yearOfDeath)) {
+//          return oldest;
+//       } else {
+//          return current;
+//       }
+//    });
+// };
+
+const getAge = function (birth, death) {
+   if (!death) {
+      death = new Date().getFullYear();
+   }
+   return death - birth;
+};
 
 module.exports = findTheOldest
